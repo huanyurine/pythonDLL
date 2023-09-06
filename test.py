c3057fd1ec8315b3fcf7a76ebbcfddbfa1a21f06
huanyurine <65449916+huanyurine@users.noreply.github.com>
2023-09-06 19:04:06 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import ctypes
-from utils_dllclass import DllClass
+from py_utils.utils_dllclass import DllClass
 
 DllClass.dll_link("DLLExample.dll")
 # DllClass.dll.add, add is an int(int, int) function name in C++
 dll_obj = DllClass(func=DllClass.dll.add, res_type=ctypes.c_int, arg_types=[ctypes.c_int, ctypes.c_int])
-obj = dll_obj.func(1, 2)
+obj = dll_obj.func(1, 72)
 print(obj)
